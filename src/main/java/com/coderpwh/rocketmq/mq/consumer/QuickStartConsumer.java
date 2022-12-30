95a2194d3c5979062c1a1609a599df290d940d49
coderpwh <pwhfbc@outlook.com>
2022-12-30 17:35:08 +0800

    fix:优化代码

--- src/main/java/com/coderpwh/rocketmq/mq/consumer/QuickStartConsumer.java
+++ src/main/java/com/coderpwh/rocketmq/mq/consumer/QuickStartConsumer.java
@@ -19,7 +19,7 @@ public class QuickStartConsumer {
     private static Logger logger = LoggerFactory.getLogger(QuickStartConsumer.class);
 
     public static final String CONSUMER_GROUP = "please_rename_unique_group_name_4";
-    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
+    public static final String DEFAULT_NAMESRVADDR = "120.79.226.167:9876";
     public static final String TOPIC = "TopicTest";
 
 
--- src/main/java/com/coderpwh/rocketmq/mq/producer/QuickStartProducer.java
+++ src/main/java/com/coderpwh/rocketmq/mq/producer/QuickStartProducer.java
@@ -22,7 +22,7 @@ public class QuickStartProducer {
 
     public static final int MESSAGE_COUNT = 1000;
     public static final String PRODUCER_GROUP = "please_rename_unique_group_name";
-    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
+    public static final String DEFAULT_NAMESRVADDR = "120.79.226.167:9876";
     public static final String TOPIC = "TopicTest";
     public static final String TAG = "TagA";
 
