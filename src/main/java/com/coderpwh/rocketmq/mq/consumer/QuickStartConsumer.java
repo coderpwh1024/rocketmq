bdeab7f07cbeb6a9e0046c2cde5ecc7b6ab04a3c
coderpwh <pwhfbc@outlook.com>
2022-12-30 11:25:21 +0800

    feat:finish quickStartConsumer

---
+++ src/main/java/com/coderpwh/rocketmq/mq/consumer/QuickStartConsumer.java
@@ -0,0 +1,41 @@
+package com.coderpwh.rocketmq.mq.consumer;
+
+import com.coderpwh.rocketmq.mq.producer.QuickStartProducer;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author coderpwh
+ * @date 2022/12/30 11:08
+ */
+public class QuickStartConsumer {
+
+
+    private static Logger logger = LoggerFactory.getLogger(QuickStartConsumer.class);
+
+    public static final String CONSUMER_GROUP = "please_rename_unique_group_name_4";
+    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
+    public static final String TOPIC = "TopicTest";
+
+
+    public void consumer() {
+        try {
+            DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
+            consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+            consumer.subscribe(TOPIC, "*");
+            consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            });
+            consumer.start();
+            System.out.printf("Consumer Started.%n");
+        } catch (Exception e) {
+            logger.error("方法[consumer]异常信息为:{}", e.getMessage());
+        }
+    }
+
+}
