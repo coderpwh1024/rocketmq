33c21d363ff8a384b0f1cbd37f72ec744a127502
coderpwh <pwhfbc@outlook.com>
2023-02-15 17:01:04 +0800

    feat:add TransactionListenerImpl class

--- src/main/java/com/coderpwh/rocketmq/mq/consumer/TransactionListenerImpl.java
+++ src/main/java/com/coderpwh/rocketmq/mq/consumer/TransactionListenerImpl.java
@@ -5,6 +5,9 @@ import org.apache.rocketmq.client.producer.TransactionListener;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author coderpwh
  * @date 2023/2/14 16:02
@@ -12,22 +15,35 @@ import org.apache.rocketmq.common.message.MessageExt;
 
 public class TransactionListenerImpl implements TransactionListener {
 
+    private AtomicInteger transactionIndex = new AtomicInteger(0);
 
-    public static void consumer() {
+    private ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
 
-    }
-
-    public static void main(String[] args) {
-
-    }
 
     @Override
-    public LocalTransactionState executeLocalTransaction(Message message, Object o) {
-        return null;
+    public LocalTransactionState executeLocalTransaction(Message message, Object obj) {
+        int value = transactionIndex.getAndIncrement();
+        int status = value % 3;
+        localTrans.put(message.getTransactionId(), status);
+        return LocalTransactionState.UNKNOW;
     }
 
     @Override
     public LocalTransactionState checkLocalTransaction(MessageExt messageExt) {
-        return null;
+
+        Integer status = localTrans.get(messageExt.getTransactionId());
+        if (null != status) {
+            switch (status) {
+                case 0:
+                    return LocalTransactionState.UNKNOW;
+                case 1:
+                    return LocalTransactionState.COMMIT_MESSAGE;
+                case 2:
+                    return LocalTransactionState.ROLLBACK_MESSAGE;
+                default:
+                    return LocalTransactionState.COMMIT_MESSAGE;
+            }
+        }
+        return LocalTransactionState.COMMIT_MESSAGE;
     }
 }
