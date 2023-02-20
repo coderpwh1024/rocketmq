5998935c9ba41d926611e2c001c8fa86d6abff0b
coderpwh <pwhfbc@outlook.com>
2023-02-20 10:57:30 +0800

    feat:add SqlFilterConsumer class

---
+++ src/main/java/com/coderpwh/rocketmq/mq/consumer/SqlFilterConsumer.java
@@ -0,0 +1,53 @@
+package com.coderpwh.rocketmq.mq.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.MessageSelector;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author coderpwh
+ * @date 2023/2/20 10:47
+ */
+public class SqlFilterConsumer {
+
+
+    private static Logger logger = LoggerFactory.getLogger(SqlFilterConsumer.class);
+
+    public static final String CONSUMER_GROUP = "please_rename_unique_group_name";
+    public static final String DEFAULT_NAMESRVADDR = "120.79.226.167:9876";
+    public static final String TOPIC = "SqlFilterTest";
+
+
+    public static void consumer() {
+        try {
+            DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
+
+            consumer.subscribe(TOPIC, MessageSelector.bySql("(TAGS is not null and TAGS in ('TagA','TagB'))" + "and (a is not null and a between 0 and 3)"));
+
+            consumer.registerMessageListener(new MessageListenerConcurrently() {
+                @Override
+                public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                    logger.info("Receive new Messages,线程名:{},消息体:{}", Thread.currentThread().getName(), list);
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+            });
+            consumer.start();
+        } catch (Exception e) {
+            logger.error("Sql消费异常,异常消息为:{}", e.getMessage());
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        consumer();
+    }
+
+}
