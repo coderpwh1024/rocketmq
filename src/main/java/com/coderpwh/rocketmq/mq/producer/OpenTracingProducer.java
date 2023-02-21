6d3f0a2a713e67d2ea8a9d7a3be20b4ec5884650
coderpwh <pwhfbc@outlook.com>
2023-02-21 15:22:30 +0800

    fix:update OpenTracingProducer class

--- src/main/java/com/coderpwh/rocketmq/mq/producer/OpenTracingProducer.java
+++ src/main/java/com/coderpwh/rocketmq/mq/producer/OpenTracingProducer.java
@@ -1,10 +1,15 @@
 package com.coderpwh.rocketmq.mq.producer;
 
+import com.alibaba.fastjson.JSON;
 import io.jaegertracing.Configuration;
 import io.jaegertracing.internal.samplers.ConstSampler;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.trace.hook.SendMessageOpenTracingHookImpl;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,12 +35,26 @@ public class OpenTracingProducer {
 
     public static void main(String[] args) {
 
-        Tracer tracer = initTracer();
 
-        DefaultMQProducer producer = new DefaultMQProducer(PRODUCER_GROUP);
-        producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-        producer.setCreateTopicKey("TBW102");
-        producer.setSendMsgTimeout(60000);
+        try {
+            Tracer tracer = initTracer();
+
+            DefaultMQProducer producer = new DefaultMQProducer(PRODUCER_GROUP);
+            producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
+            producer.setCreateTopicKey("TBW102");
+            producer.setSendMsgTimeout(60000);
+            producer.getDefaultMQProducerImpl().registerSendMessageHook(new SendMessageOpenTracingHookImpl(tracer));
+            producer.start();
+
+            Message msg = new Message(TOPIC, TAG, KEY, "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+            SendResult sendResult = producer.send(msg);
+            logger.info("发送结果为：{}", JSON.toJSONString(sendResult));
+
+            producer.shutdown();
+        } catch (Exception e) {
+            logger.error("异常消息为:{}", e.getMessage());
+        }
 
 
     }
