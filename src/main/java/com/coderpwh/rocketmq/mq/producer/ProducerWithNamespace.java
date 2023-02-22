e50ec100485a8a77f2e2a24a3736dc97643f921f
coderpwh <pwhfbc@outlook.com>
2023-02-22 11:18:10 +0800

    feat:Init ProducerWithNamespace class

---
+++ src/main/java/com/coderpwh/rocketmq/mq/producer/ProducerWithNamespace.java
@@ -0,0 +1,26 @@
+package com.coderpwh.rocketmq.mq.producer;
+
+/**
+ * @author coderpwh
+ * @date 2023/2/22 11:12
+ */
+public class ProducerWithNamespace {
+
+    public static final String NAMESPACE = "InstanceTest";
+
+    public static final String PRODUCER_GROUP = "pidTest";
+
+    public static final String DEFAULT_NAMESRVADDR = "120.79.226.167:9876";
+
+    public static final int MESSAGE_COUNT = 100;
+
+    public static final String TOPIC = "NAMESPACE_TOPIC";
+
+    public static final String TAG = "tagTest";
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
