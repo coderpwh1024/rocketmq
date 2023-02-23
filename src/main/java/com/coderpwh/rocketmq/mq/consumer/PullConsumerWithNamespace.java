88605c3cf5544358f460df6d8b89242420c6a3d0
coderpwh <pwhfbc@outlook.com>
2023-02-23 09:52:36 +0800

    Init PullConsumerWithNamespace class

---
+++ src/main/java/com/coderpwh/rocketmq/mq/consumer/PullConsumerWithNamespace.java
@@ -0,0 +1,18 @@
+package com.coderpwh.rocketmq.mq.consumer;
+
+/**
+ * @author coderpwh
+ * @date 2023/2/22 17:40
+ */
+public class PullConsumerWithNamespace {
+
+    public static final String NAMESPACE = "InstanceTest";
+
+    public static final String CONSUMER_GROUP = "cidTest";
+
+    public static final String DEFAULT_NAMESRVADDR = "120.79.226.167:9876";
+
+    public static final String TOPIC = "NAMESPACE_TOPIC";
+    
+
+}
