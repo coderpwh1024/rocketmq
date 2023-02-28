72f3e4e749793be96c332e3990a202d7c9f7e7da
coderpwh <pwhfbc@outlook.com>
2023-02-28 16:36:57 +0800

    fix:update PullConsumerWithNamespace class

--- src/main/java/com/coderpwh/rocketmq/mq/consumer/PullConsumerWithNamespace.java
+++ src/main/java/com/coderpwh/rocketmq/mq/consumer/PullConsumerWithNamespace.java
@@ -1,12 +1,15 @@
 package com.coderpwh.rocketmq.mq.consumer;
 
-import apache.rocketmq.v2.MessageQueue;
+import com.alibaba.fastjson.JSON;
+import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
 import org.apache.rocketmq.client.consumer.PullResult;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author coderpwh
@@ -26,6 +29,51 @@ public class PullConsumerWithNamespace {
     public static final String TOPIC = "NAMESPACE_TOPIC";
 
     private static final Map<MessageQueue, Long> OFFSET_TABLE = new HashMap<>();
+
+
+    public static void consumer() {
+
+        try {
+            DefaultMQPullConsumer pullConsumer = new DefaultMQPullConsumer(NAMESPACE, CONSUMER_GROUP);
+            pullConsumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
+            pullConsumer.start();
+            Set<MessageQueue> mqs = pullConsumer.fetchSubscribeMessageQueues(TOPIC);
+
+            for (MessageQueue mq : mqs) {
+                logger.info("Consume from the topic:{} queue:{}", mq.getTopic(), mq);
+                SINGLE_MQ:
+                while (true) {
+
+                    PullResult pullResult =
+                            pullConsumer.pullBlockIfNotFound(mq, null, getMessageQueueOffset(mq), 32);
+                    logger.info("消费结果为:{}", JSON.toJSONString(pullResult));
+                    putMessageQueueOffset(mq, pullResult.getNextBeginOffset());
+                    switch (pullResult.getPullStatus()) {
+                        case FOUND:
+                            dealWithPullResult(pullResult);
+                            break;
+                        case NO_MATCHED_MSG:
+                            break;
+                        case NO_NEW_MSG:
+                            break SINGLE_MQ;
+                        case OFFSET_ILLEGAL:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            }
+
+        } catch (Exception e) {
+            logger.error("消息消费异常,异常消息为:{}", e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        consumer();
+    }
 
 
     private static long getMessageQueueOffset(MessageQueue mq) {
