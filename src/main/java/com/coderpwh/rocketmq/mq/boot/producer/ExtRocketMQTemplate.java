8b7a0e7044ede0d4e73ee7568008abc2748a3678
coderpwh <pwhfbc@outlook.com>
2023-03-06 17:38:58 +0800

    fix:update ExtRocketMQTemplate class

--- src/main/java/com/coderpwh/rocketmq/mq/boot/producer/ExtRocketMQTemplate.java
+++ src/main/java/com/coderpwh/rocketmq/mq/boot/producer/ExtRocketMQTemplate.java
@@ -1,8 +1,16 @@
 package com.coderpwh.rocketmq.mq.boot.producer;
 
+import org.apache.rocketmq.spring.annotation.ExtRocketMQTemplateConfiguration;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+
 /**
+ * RocketMQTemplate 额外配置类
+ *
  * @author coderpwh
  * @date 2023/3/6 17:22
  */
-public class ExtRocketMQTemplate {
+@ExtRocketMQTemplateConfiguration(nameServer = "${demo.rocketmq.extNameServer}", tlsEnable = "${demo.rocketmq.ext.useTLS}", instanceName = "pztest33")
+public class ExtRocketMQTemplate extends RocketMQTemplate {
+
+
 }
