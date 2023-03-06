d6602a8bde0bcaa36337d916247ececa649ebaaf
coderpwh <pwhfbc@outlook.com>
2023-03-06 16:50:35 +0800

    fix:update ProducerServiceImpl class

--- src/main/java/com/coderpwh/rocketmq/service/impl/ProducerServiceImpl.java
+++ src/main/java/com/coderpwh/rocketmq/service/impl/ProducerServiceImpl.java
@@ -2,6 +2,7 @@ package com.coderpwh.rocketmq.service.impl;
 
 import com.coderpwh.rocketmq.service.ProducerService;
 import com.coderpwh.rocketmq.util.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,7 +12,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProducerServiceImpl implements ProducerService {
 
+
+    @Value("${demo.rocketmq.transTopic}")
+    private String springTransTopic;
+
+    @Value("${demo.rocketmq.topic}")
+    private String springTopic;
+
+    @Value("${demo.rocketmq.topic.user}")
+    private String userTopic;
+
+    @Value("${demo.rocketmq.orderTopic}")
+    private String orderPaidTopic;
+
+    @Value("${demo.rocketmq.msgExtTopic}")
+    private String msgExtTopic;
+
+
+    @Value("${demo.rocketmq.bytesRequestTopic}")
+    private String stringRequestTopic;
+
+
+    @Value("${demo.rocketmq.objectRequestTopic}")
+    private String objectRequestTopic;
+
+    @Value("${demo.rocketmq.genericRequestTopic}")
+    private String genericRequestTopic;
+    
+
     @Override
+
     public Result testProducer() {
         return null;
     }
