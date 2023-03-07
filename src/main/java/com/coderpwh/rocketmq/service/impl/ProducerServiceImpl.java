35f826d91969846e06448d7e0f92018d70da7404
coderpwh <pwhfbc@outlook.com>
2023-03-07 10:26:38 +0800

    fix:update ExtRocketMQyTemplate use

--- src/main/java/com/coderpwh/rocketmq/service/impl/ProducerServiceImpl.java
+++ src/main/java/com/coderpwh/rocketmq/service/impl/ProducerServiceImpl.java
@@ -2,7 +2,7 @@ package com.coderpwh.rocketmq.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.coderpwh.rocketmq.domain.User;
-import com.coderpwh.rocketmq.mq.consumer.OpenTracingPushConsumer;
+import com.coderpwh.rocketmq.mq.boot.producer.ExtRocketMQTemplate;
 import com.coderpwh.rocketmq.service.ProducerService;
 import com.coderpwh.rocketmq.util.Result;
 import org.apache.rocketmq.client.producer.SendResult;
@@ -10,7 +10,11 @@ import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 
 import javax.annotation.Resource;
 
@@ -56,8 +60,19 @@ public class ProducerServiceImpl implements ProducerService {
     private RocketMQTemplate rocketMQTemplate;
 
 
+    @Resource
+    private ExtRocketMQTemplate extRocketMQTemplate;
+
+
     @Override
     public Result testProducer() {
+        testSpringTopic();
+
+        testUserTopic();
+
+        testUserTopicByWithPayload();
+
+
         return null;
     }
 
@@ -80,6 +95,30 @@ public class ProducerServiceImpl implements ProducerService {
         user.setUserName("coderpwh");
         SendResult sendResult = rocketMQTemplate.syncSend(userTopic, user);
         logger.info("方法[testUserTopic]中 topic:{},结果为:{}", userTopic, JSON.toJSONString(sendResult));
+        return Result.ok();
+    }
+
+
+    public Result testUserTopicByWithPayload() {
+        User user = new User();
+        user.setUserAge(18);
+        user.setUserName("coderpwh");
+        Message<User> message = MessageBuilder.withPayload(user).setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON_VALUE).build();
+
+        SendResult sendResult = rocketMQTemplate.syncSend(userTopic, message);
+        logger.info("方法testUserTopicByWithPayload中,topic为:{},发送结果为:{}", userTopic, JSON.toJSONString(sendResult));
+        return Result.ok();
+    }
+
+
+    public Result testSpringTopicByWithPayload() {
+
+        Message<byte[]> message = MessageBuilder.withPayload("Hello,World 2023!".getBytes()).build();
+
+        SendResult sendResult = extRocketMQTemplate.syncSend(springTopic, message);
+
+        logger.info("testSpringTopicByWithPayload方法中,topic为:{},发送结果为:{}", springTopic, JSON.toJSONString(sendResult));
+
         return Result.ok();
     }
 
