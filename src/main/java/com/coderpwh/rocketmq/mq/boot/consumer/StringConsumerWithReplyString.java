9917d85c480264da4a09662c3045dafbd88b97d5
coderpwh <pwhfbc@outlook.com>
2023-03-10 10:40:38 +0800

    feat:Init StringConsumerWithReplyString class

---
+++ src/main/java/com/coderpwh/rocketmq/mq/boot/consumer/StringConsumerWithReplyString.java
@@ -0,0 +1,24 @@
+package com.coderpwh.rocketmq.mq.boot.consumer;
+
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQReplyListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author coderpwh
+ * @date 2023/3/10 10:14
+ */
+@RocketMQMessageListener(topic = "${demo.rocketmq.stringRequestTopic}", consumerGroup = "${demo.rocketmq.stringRequestConsumer}", selectorExpression = "${demo.rocketmq.tag}", replyTimeout = 10000)
+public class StringConsumerWithReplyString implements RocketMQReplyListener<String, String> {
+
+
+    private static Logger logger = LoggerFactory.getLogger(StringConsumer.class);
+
+    @Override
+    public String onMessage(String s) {
+        logger.info("StringConsumerWithReplyString 接收到消息为:{}", s);
+        return "reply String";
+    }
+
+}
