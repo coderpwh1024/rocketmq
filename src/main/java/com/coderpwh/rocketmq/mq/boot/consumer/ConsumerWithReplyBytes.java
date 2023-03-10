96670cd05579d58609a7b82e420d17e1f2fca53d
coderpwh <pwhfbc@outlook.com>
2023-03-10 10:50:25 +0800

    feat: Init ConsumerWithReplyBytes class

---
+++ src/main/java/com/coderpwh/rocketmq/mq/boot/consumer/ConsumerWithReplyBytes.java
@@ -0,0 +1,28 @@
+package com.coderpwh.rocketmq.mq.boot.consumer;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQReplyListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author coderpwh
+ * @date 2023/3/10 10:43
+ */
+@RocketMQMessageListener(topic = "${demo.rocketmq.bytesRequestTopic}", consumerGroup = "${demo.rocketmq.bytesRequestConsumer}", selectorExpression = "${demo.rocketmq.tag}")
+public class ConsumerWithReplyBytes implements RocketMQReplyListener<MessageExt, byte[]> {
+
+
+    private static Logger logger = LoggerFactory.getLogger(ConsumerWithReplyBytes.class);
+
+    @Override
+    public byte[] onMessage(MessageExt messageExt) {
+        String str = "reply message content";
+        logger.info("ConsumerWithReplyBytes 接收到消息为:{}", JSON.toJSONString(messageExt));
+
+        return str.getBytes();
+    }
+
+}
