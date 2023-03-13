89ceecb3cb7d1b92f994db4caab6a45a0761a07d
coderpwh <pwhfbc@outlook.com>
2023-03-13 11:19:38 +0800

    fix: testTransaction

--- src/main/java/com/coderpwh/rocketmq/service/impl/AclProducerServiceImpl.java
+++ src/main/java/com/coderpwh/rocketmq/service/impl/AclProducerServiceImpl.java
@@ -2,7 +2,17 @@ package com.coderpwh.rocketmq.service.impl;
 
 import com.coderpwh.rocketmq.service.AclProducerService;
 import com.coderpwh.rocketmq.util.Result;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.springframework.messaging.Message;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * @author coderpwh
@@ -12,9 +22,44 @@ import org.springframework.stereotype.Service;
 public class AclProducerServiceImpl implements AclProducerService {
 
 
+    private static Logger logger = LoggerFactory.getLogger(AclProducerServiceImpl.class);
+
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+
+    @Value("${demo.rocketmq.normal.transTopic}")
+    private String springTransTopic;
+
+
+    @Value("${demo.rocketmq.normal.topic}")
+    private String springTopic;
+
     @Override
+
     public Result testAclProducer() {
         return null;
     }
+
+
+    public void testTransaction() {
+        String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                Message msg = MessageBuilder.withPayload("Hello RocketMQ " + i).setHeader(RocketMQHeaders.TRANSACTION_ID, "KEY_" + i).build();
+
+                SendResult sendResult = rocketMQTemplate.sendMessageInTransaction(springTransTopic + ":" + tags[i % tags.length], msg, null);
+                logger.info("testTransaction方法,发送消息内容为:{},发送结果为:{}", msg.getPayload(), sendResult.getSendStatus());
+
+                Thread.sleep(10);
+            }
+        } catch (Exception e) {
+            logger.error("testTransaction方法 发送事务消息异常,异常消息为:{}", e.getMessage());
+        }
+
+
+    }
+
 
 }
